1230e4ae5c07d85b0133868506a21c4089a98717
Roxana Soto <roxanna.sa@gmail.com>
2023-04-18 21:49:53 -0400

    cambios 18 abril 2023

--- src/index.js
+++ src/index.js
@@ -33,5 +33,58 @@ function dibujoNumeroTarjeta (){
     }
 }
 
+function dibujoNombreTarjeta (){
+    let creditCardName = document.querySelector("#inputNombre").value;
+    if(creditCardName.length == 0){
+        document.querySelector("#nombre").innerHTML = "LOREM IPSUM";
+    } else { 
+        document.querySelector("#nombre").innerHTML = creditCardName;
+
+    }
+}
+
+function botonValidacion(){
+    const creditCardNumber = document.querySelector("#inputNumero").value; 
+    const isValid = validator.isValid(creditCardNumber);
+    if (isValid){
+        alert('tarjeta válida');
+    } else {
+        alert ('tarjeta inválida');
+    }
+}
+
 document.querySelector("#inputNumero").addEventListener('keydown', onlyNumbers);
 document.querySelector("#inputNumero").addEventListener('keyup', dibujoNumeroTarjeta);
+document.querySelector("#inputNombre").addEventListener('keyup', dibujoNombreTarjeta);
+document.querySelector("#botonEnvio").addEventListener('click',botonValidacion);
+
+
+const tarjeta = document.querySelector("#tarjeta");
+const formulario = document.querySelector("#formulario");
+
+// Giro de la tarjeta
+const inputCvv = document.querySelector("#inputCVV");
+inputCvv.addEventListener('focus', () => {
+	tarjeta.classList.add('active');
+});
+inputCvv.addEventListener('blur', () => {
+	tarjeta.classList.remove('active');
+});
+
+
+//selección del mes de expiración
+for(let i = 1; i <= 12; i++){
+	let opcion = document.createElement('option');
+	opcion.value = i;
+	opcion.innerHTML = i;
+	formulario.seleccionarMes.appendChild(opcion); //agrega hijos (opciones) al elemento padre (opcion)
+}
+
+//selección del año de expiración
+const anioActual = new Date().getFullYear();
+for(let i = anioActual; i <= anioActual + 10; i++){
+	let opcion = document.createElement('option'); //me crea mas elementos de tag html, en este caso de <option>
+	opcion.value = i;
+	opcion.innerHTML = i;
+	formulario.seleccionarAnio.appendChild(opcion);
+}
